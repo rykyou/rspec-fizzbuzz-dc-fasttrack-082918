032f682bec9dbe15130e06c877a215a9be2eb3e7
Robin Kim <rkim1214@gmail.com>
2018-10-01 15:18:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if number % 15 == 0 
+  if number % 5 == 0 && number % 3 == 0
     "FizzBuzz"
   elsif number % 5 == 0 
     "Buzz"
