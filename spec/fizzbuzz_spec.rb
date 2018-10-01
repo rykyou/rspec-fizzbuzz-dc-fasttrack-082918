a3d598195f7c646fe7bf902490a156a974cbd14e
Robin Kim <rkim1214@gmail.com>
2018-10-01 12:30:03 +0000

    Done.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,6 @@
-require_relative './spec_helper.rb'
-# We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
+require_relative './spec_helper.rb' #telling fizzbuzz_spec.rb to load both 
+require_relative '../fizzbuzz.rb'   #spec_helper.rb and fizzbuzz.rb
+ 
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
